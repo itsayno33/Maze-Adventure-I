898c06f8b60d83e3b525b0fc09a997850d9d4ceb
itsayno33 <itsayno33@gmail.com>
2023-12-26 11:58:10 +0900

    webpack.json中に残しておいた以前の設定を削除。

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,11 @@ const path = require('path');
 module.exports = (env) => {
 	return {
 		mode: 'development',
-//		entry: './src/index.ts', //ファイルをまとめる際のエントリーポイント
 		entry: env.entry, //ファイルをまとめる際のエントリーポイント
 		devtool: 'inline-source-map',
 		output: {
 			filename: 'bundle.js', //まとめた結果出力されるファイル名
 //			path: path.resolve(__dirname, 'dist')
-//			path: 'T:/Apache24Root/mai/mai_maze/js'
 			path: env.outdir
 	},
 		resolve: {
