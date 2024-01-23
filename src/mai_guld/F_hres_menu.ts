551db82cd97f166fd0464af7753a399303fec718
itsayno33 <itsayno33@gmail.com>
2024-01-23 21:32:51 +0900

    冒険者情報画面のバグ修正

--- src/mai_guld/F_hres_menu.ts
+++ src/mai_guld/F_hres_menu.ts
@@ -14,7 +14,9 @@ import { _ceil, _floor, _min, _round } from "../common/F_Math";
 import { C_Hero }                      from "../common/C_Hero";
 import { T_MakeEnumType }              from "../common/T_MakeEnumType";
 import { g_mvm, g_team, g_guld }       from "./global_for_guild";
-import { _alert } from "../common/global";
+import { _alert, g_mes } from "../common/global";
+import { get_new_hero } from '../common/F_load_and_save';
+import { promises } from 'dns';
 
 let dom_view_switch : HTMLDivElement;
 
@@ -23,6 +25,9 @@ let dom_team_list:    HTMLUListElement;
 
 let dom_guld_fields : HTMLFieldSetElement;
 let dom_guld_list:    HTMLUListElement;
+
+let dom_appd_fields : HTMLFieldSetElement;
+let dom_appd_list:    HTMLUListElement;
 
 let dom_menu_fields : HTMLFieldSetElement;
 let dom_menu_list:    HTMLUListElement;
@@ -35,22 +40,28 @@ let dom_hero_detail : HTMLUListElement;
 
 let team_list: C_Hero[];
 let guld_list: C_Hero[];
+let appd_list: C_Hero[];
 let hero_detail: {[key: string]: HTMLLIElement};
+
+let new_hres: C_Hero[] = [];
 
 type T_menu_list = {id: string, title: string, fnc: ()=>void}[];
 let menu_list_for_team: T_menu_list;
 let menu_list_for_guld: T_menu_list;
+let menu_list_for_appd: T_menu_list;
 
 let inpt_name_list: {[id: string]: {id: string, label: HTMLLabelElement, input: HTMLInputElement}};
 
 let team_list_cols: number;
 let guld_list_cols: number;
+let appd_list_cols: number;
 let menu_list_cols: number;
 
 const T_TG_mode: {[kind: string]: number}  = {
     Hide: 0,
     Team: 1,
     Guld: 2,
+    Appd: 3,
 } as const;
 type T_TG_mode = T_MakeEnumType<typeof T_TG_mode>;
 let TG_mode: T_TG_mode;
@@ -59,9 +70,10 @@ const T_SubView: {[kind: string]: number}  = {
     Hide: 0,
     Team: 1,
     Guld: 2,
-    Menu: 3,
-    IpNm: 5,
-    IpCk: 6,
+    Appd: 3,
+    Menu: 5,
+    IpNm: 6,
+    IpCk: 7,
 } as const;
 type T_SubView = T_MakeEnumType<typeof T_SubView>;
 
@@ -71,6 +83,7 @@ let cursor: T_cursor;
 let cursor_Hide: T_cursor;
 let cursor_Team: T_cursor;
 let cursor_Guld: T_cursor;
+let cursor_Appd: T_cursor;
 let cursor_Menu: T_cursor;
 let cursor_IpNm: T_cursor;
 let cursor_IpCk: T_cursor;
@@ -122,6 +135,7 @@ function update_all() {
 function init_data_list(): boolean {
     if (!init_team_list()) return false;
     if (!init_guld_list()) return false;
+    if (!init_appd_list()) return false;
     if (!init_menu_list()) return false;
     return true;
 }
@@ -134,6 +148,11 @@ function init_guld_list(): boolean {
     update_guld_list(); 
     return true;
 }
+function init_appd_list(): boolean { 
+    appd_list = [];
+//    update_appd_list(); 
+    return true;
+}
 function init_menu_list():boolean { 
     menu_list_for_team = [
         {id: 'name',  fnc: _go_ipnm, title: '名前を変える'},
@@ -143,6 +162,11 @@ function init_menu_list():boolean {
         {id: 'name',  fnc: _go_ipnm, title: '名前を変える'},
         {id: 'join',  fnc: _go_join, title: 'チームに入れる'},
         {id: 'fire',  fnc: _go_fire, title: 'ギルドをクビにする'},
+    ]; 
+    menu_list_for_appd = [
+        {id: 'name',  fnc: _go_ipnm, title: '名前を変える'},
+        {id: 'adpt',  fnc: _go_adpt, title: 'ギルドに採用する'},
+        {id: 'away',  fnc: _go_away, title: '追い返す！'},
     ]; 
     return true;
 }
@@ -184,15 +208,36 @@ function _go_fire(): void {
     display_default_message();
     hres_ctls.act("hres_fire");
 }
+function _go_adpt(): void {
+    if (!exist_appd()) return;
+    if (max_of_guld()) {
+        g_mvm.notice_message('ギルドが満員です。誰かクビにしてください');
+        return;
+    }
+
+    subview_act(T_SubView.IpCk);
+    mode = 'adpt';
+    display_default_message();
+    hres_ctls.act("hres_adpt");
+}
+function _go_away(): void {
+    if (!exist_appd()) return;
+
+    subview_act(T_SubView.IpCk);
+    mode = 'away';
+    display_default_message();
+    hres_ctls.act("hres_away");
+}
 
 
-function update_data_list() { 
+function update_data_list(): void { 
     update_team_list(); 
     update_guld_list(); 
+    update_appd_list(); 
     update_menu_list(); 
 }
 
-function update_team_list(){
+function update_team_list(): void {
     team_list = []; 
     for (let hero of g_team.hres()) team_list.push(hero);
 }
@@ -203,7 +248,7 @@ function max_of_team(): boolean {
     return team_list.length > 3;
 }
 
-function update_guld_list(){
+function update_guld_list(): void {
     guld_list = []; 
     for (let hero of g_guld.myteam.hres()) guld_list.push(hero);
 }
@@ -214,9 +259,32 @@ function max_of_guld(): boolean {
     return guld_list.length > 99;
 }
 
-function update_menu_list(){}
+function update_appd_list(): void {
+//    appd_list = [];
+    if (appd_list.length < 1) _get_appd_list();
+}
+async function _get_appd_list(): Promise<void> {
+    return await get_new_hero(8)?.then((jsonObj:any)=>{
+        if (jsonObj.hres === undefined) {
+            g_mes.warning_message('不正なデータを受信しました' + jsonObj.emsg);
+            _alert(jsonObj.emsg);
+            return;
+        }
+        for (let hero_data of jsonObj.hres) {
+            appd_list.push(new C_Hero(hero_data));
+        }
+    })
+}
+function exist_appd(): boolean {
+    return appd_list.length > 0;
+}
+function max_of_appd(): boolean {
+    return appd_list.length > 99;
+}
+
+function update_menu_list(): void {}
 function exist_menu(): boolean {
-    return _min([menu_list_for_team.length, menu_list_for_guld.length]) > 0;
+    return _min([menu_list_for_team.length, menu_list_for_guld.length, menu_list_for_appd.length]) > 0;
 }
 
 
@@ -229,6 +297,7 @@ function init_view(): boolean {
     if (!init_dom_view_hres()) return false; 
     if (!init_dom_team_list()) return false; 
     if (!init_dom_guld_list()) return false; 
+    if (!init_dom_appd_list()) return false; 
     if (!init_dom_menu_list()) return false; 
     if (!init_dom_inpt_list()) return false; 
     if (!init_dom_hero_detail()) return false; 
@@ -239,6 +308,7 @@ function update_view() {
     update_dom_view_hres();
     update_dom_team_list();
     update_dom_guld_list();
+    update_dom_appd_list();
     update_dom_menu_list();
 //    update_dom_inpt_list();
     update_dom_hero_detail();
@@ -248,6 +318,7 @@ function clear_view() {
     clear_dom_view_hres();
     clear_dom_team_list();
     clear_dom_guld_list();
+    clear_dom_appd_list();
     clear_dom_menu_list();
     clear_dom_inpt_list();
     clear_dom_hero_detail();
@@ -374,6 +445,55 @@ function clear_dom_guld_list():void  {
 
 
 // ******************************
+// 新人募集リスト表示　関係
+// ******************************
+
+function init_dom_appd_list(): boolean {
+    // 冒険者情報
+    try {
+        dom_appd_fields = document.getElementById('hres_appd_fields') as HTMLFieldSetElement;
+        dom_appd_list   = document.getElementById('appd_list')        as HTMLUListElement;
+    } catch (err) {
+        return false;
+    }
+    if (dom_appd_fields === null) return false;
+    if (dom_appd_list   === null) return false;
+
+    dom_appd_fields.style.display = 'none';
+    return true;
+
+}
+function update_dom_appd_list(): void {
+    clear_dom_appd_list();
+    for (let ii in appd_list) {
+        const li = document.createElement('li') as HTMLLIElement;
+        li.innerHTML = `${appd_list[ii].name()}<p></p>`;
+
+        li.id = ii.toString();
+        li.addEventListener("click", _OK_appd_Fnc, false);
+        dom_appd_list.appendChild(li);
+    }
+    list_high_light_on();
+}
+function _OK_appd_Fnc(this: HTMLLIElement, e: MouseEvent): void {
+    subview_act(T_SubView.Appd);
+    cursor.idx  = Number(this.id); 
+
+    list_high_light_on(); 
+    update_dom_hero_detail();
+
+    isOK();
+//    display_default_message();
+}
+
+function clear_dom_appd_list():void  {
+    while (dom_appd_list.firstChild !== null) {
+        dom_appd_list.removeChild(dom_appd_list.firstChild);
+    }
+}
+
+
+// ******************************
 // サブメニュー表示　関係
 // ******************************
 
@@ -398,6 +518,7 @@ function update_dom_menu_list(): void {
     switch (TG_mode) {
         case T_TG_mode.Team: menu_list = menu_list_for_team;break;
         case T_TG_mode.Guld: menu_list = menu_list_for_guld;break;
+        case T_TG_mode.Appd: menu_list = menu_list_for_appd;break;
         default: return;
     }
     for (let ii in menu_list) {
@@ -489,6 +610,9 @@ function update_dom_ipnm(): void {
         case T_TG_mode.Guld: 
             name_input.value = guld_list[cursor_Guld.idx].name();
             break;
+        case T_TG_mode.Appd: 
+            name_input.value = appd_list[cursor_Appd.idx].name();
+            break;
         default: return;
     }
 
@@ -534,6 +658,9 @@ function update_dom_hero_detail() {
         case T_SubView.Guld:
             hero_info_form_set(guld_list, hero_detail, cursor.idx);
             break;
+        case T_SubView.Appd:
+            hero_info_form_set(appd_list, hero_detail, cursor.idx);
+            break;
     }
 }
 
@@ -565,6 +692,8 @@ function init_default_ctls(): boolean {
         if (!hres_ctls.add('hres_leav', hres_ctls_leav)) return false;
         if (!hres_ctls.add('hres_join', hres_ctls_join)) return false;
         if (!hres_ctls.add('hres_fire', hres_ctls_fire)) return false;
+        if (!hres_ctls.add('hres_adpt', hres_ctls_adpt)) return false;
+        if (!hres_ctls.add('hres_away', hres_ctls_away)) return false;
     } catch (err) {
         return false;
     }
@@ -624,6 +753,20 @@ const hres_ctls_fire = {
     cpOK:  isOK_fire,
     cpNG:  isNG_chek,
 }
+const hres_ctls_adpt = {
+    name: 'hres_adpt', 
+    isOK:  isOK_adpt,
+    isNG:  isNG_chek,
+    cpOK:  isOK_adpt,
+    cpNG:  isNG_chek,
+}
+const hres_ctls_away = {
+    name: 'hres_away', 
+    isOK:  isOK_away,
+    isNG:  isNG_chek,
+    cpOK:  isOK_away,
+    cpNG:  isNG_chek,
+}
 
 function update_ctls(): void {}
 
@@ -631,6 +774,7 @@ function init_cursor(): boolean {
     cursor_Hide = {kind: T_SubView.Hide, idx: 0}; 
     cursor_Team = {kind: T_SubView.Team, idx: 0}; 
     cursor_Guld = {kind: T_SubView.Guld, idx: 0}; 
+    cursor_Appd = {kind: T_SubView.Appd, idx: 0}; 
     cursor_Menu = {kind: T_SubView.Menu, idx: 0}; 
     cursor_IpNm = {kind: T_SubView.IpNm, idx: 0}; 
     cursor_IpCk = {kind: T_SubView.IpCk, idx: 0}; 
@@ -642,6 +786,10 @@ function init_cursor(): boolean {
     else if (exist_guld()) { 
         TG_mode = T_TG_mode.Guld;
         cursor  = cursor_Guld; 
+    }
+    else if (exist_appd()) { 
+        TG_mode = T_TG_mode.Appd;
+        cursor  = cursor_Appd; 
     }
     else { 
         TG_mode = T_TG_mode.Hide;
@@ -656,6 +804,7 @@ function init_cursor(): boolean {
 function subview_hide_all(): boolean {
     dom_team_fields.style.display = 'none';
     dom_guld_fields.style.display = 'none';
+    dom_appd_fields.style.display = 'none';
     dom_menu_fields.style.display = 'none';
     dom_inpt_fields.style.display = 'none';
     return true;
@@ -667,6 +816,7 @@ function subview_act(sview: T_SubView): boolean {
     switch (sview) {
         case T_SubView.Team: subview_act_team();return true;
         case T_SubView.Guld: subview_act_guld();return true;
+        case T_SubView.Appd: subview_act_appd();return true;
         case T_SubView.Menu: subview_act_menu();return true;
         case T_SubView.IpNm: subview_act_ipnm();return true;
         case T_SubView.IpCk: subview_act_ipck();return true;
@@ -689,6 +839,14 @@ function subview_act_guld() {
 
     update_view();
     dom_guld_fields.style.display = 'block';
+}
+
+function subview_act_appd() { 
+    subview_hide_all();
+    cursor  = cursor_Appd;
+
+    update_view();
+    dom_appd_fields.style.display = 'block';
 }
 
 function subview_act_menu() {
@@ -728,6 +886,7 @@ function subview_act_ipck() {
 function get_dom_list_cols(): boolean {
     get_dom_team_list_cols();
     get_dom_guld_list_cols();
+    get_dom_appd_list_cols();
     get_dom_menu_list_cols();
     return true;
 }
@@ -754,6 +913,17 @@ function get_dom_guld_list_cols(): number {
 }
 
 
+// 新人募集一覧の列数(CSSから取得)
+function get_dom_appd_list_cols(): number {
+    let __col   = window.getComputedStyle(dom_appd_list).columnCount !== '' 
+                ? window.getComputedStyle(dom_appd_list).columnCount
+                : '1';
+
+    appd_list_cols = Number(__col); 
+    return appd_list_cols;
+}
+
+
 // メニュー一覧の列数(CSSから取得)
 function get_dom_menu_list_cols(): number {
     let __col   = window.getComputedStyle(dom_menu_list).columnCount !== '' 
@@ -773,6 +943,9 @@ function list_high_light_on() {
             break; 
         case T_SubView.Guld:
             high_light_on(dom_guld_list, cursor.idx);
+            break; 
+        case T_SubView.Appd:
+            high_light_on(dom_appd_list, cursor.idx);
             break; 
         case T_SubView.Menu:
             high_light_on(dom_menu_list, cursor.idx);
@@ -838,6 +1011,8 @@ function _calc_list_length(): number {
             return dom_team_list.children.length;
         case T_SubView.Guld:
             return dom_guld_list.children.length;
+        case T_SubView.Appd:
+            return dom_appd_list.children.length;
         case T_SubView.Menu:
             return dom_menu_list.children.length;
     }
@@ -850,6 +1025,8 @@ function _calc_list_cols(): number {
             return team_list_cols;
         case T_SubView.Guld:
             return guld_list_cols;
+        case T_SubView.Appd:
+            return appd_list_cols;
         case T_SubView.Menu:
             return menu_list_cols;
     }
@@ -873,6 +1050,11 @@ function isOK(): void {
             subview_act(T_SubView.Menu);
             display_default_message();
             break;
+        case T_SubView.Appd:
+            mode = 'menu';
+            subview_act(T_SubView.Menu);
+            display_default_message();
+            break;
         case T_SubView.Menu:
             do_menu();
             break;
@@ -883,6 +1065,7 @@ function do_menu(): void {
     switch (TG_mode) {
         case T_TG_mode.Team: menu_list = menu_list_for_team; break;
         case T_TG_mode.Guld: menu_list = menu_list_for_guld; break;
+        case T_TG_mode.Appd: menu_list = menu_list_for_appd; break;
         default: return;
     }
     menu_list[cursor_Menu.idx].fnc();
@@ -906,6 +1089,10 @@ function isOK_cknm(): void {
             change_hero_name(guld_list[cursor_Guld.idx]); 
             subview_act(T_SubView.Guld);
             break;
+        case T_TG_mode.Appd: 
+            change_hero_name(appd_list[cursor_Appd.idx]); 
+            subview_act(T_SubView.Appd);
+            break;
     };
     clear_dom_inpt_list();
     go_back_view_mode('改名しました');
@@ -916,7 +1103,7 @@ function isOK_leav(): void {
 
     g_guld.myteam.add_hero(hero);
     g_team.rmv_hero(hero);
-    init_data_list();
+    update_data_list();
 
     cursor_Team.idx = 0;
     go_back_view_mode('チームから外しました');
@@ -927,7 +1114,7 @@ function isOK_join(): void {
 
     g_team.add_hero(hero);
     g_guld.myteam.rmv_hero(hero);
-    init_data_list();
+    update_data_list();
 
     cursor_Guld.idx = 0;
     go_back_view_mode('チームに入れました');
@@ -935,10 +1122,31 @@ function isOK_join(): void {
 
 function isOK_fire(): void {
     g_guld.myteam.rmv_hero(guld_list[cursor_Guld.idx]);
-    init_data_list();
+    update_data_list();
 
     cursor_Guld.idx = 0;
     go_back_view_mode('クビにしました。。。');
+}
+
+function isOK_adpt(): void {
+    const hero = appd_list[cursor_Appd.idx];
+
+    g_guld.myteam.add_hero(hero);
+    appd_list.splice(cursor_Appd.idx, 1);
+    update_data_list();
+
+    cursor_Appd.idx = 0;
+    go_back_view_mode('ギルドに採用しました');
+}
+
+function isOK_away(): void {
+    const hero = appd_list[cursor_Appd.idx];
+
+    appd_list.splice(cursor_Appd.idx, 1);
+    update_data_list();
+
+    cursor_Appd.idx = 0;
+    go_back_view_mode('叩き出しました。。。');
 }
 
 function go_back_view_mode(msg: string): void {
@@ -949,6 +1157,9 @@ function go_back_view_mode(msg: string): void {
             break;
         case T_TG_mode.Guld: 
             subview_act(T_SubView.Guld);
+            break;
+        case T_TG_mode.Appd: 
+            subview_act(T_SubView.Appd);
             break;
     }
     hres_ctls.act("hres_nor");
@@ -977,11 +1188,15 @@ function isNG(): void {
         case T_SubView.Guld:
             isRT();
             break;
+        case T_SubView.Appd:
+            isRT();
+            break;
         case T_SubView.Menu:
             mode = 'view';
             switch (TG_mode) {
                 case T_TG_mode.Team: subview_act(T_SubView.Team); break;
                 case T_TG_mode.Guld: subview_act(T_SubView.Guld); break;
+                case T_TG_mode.Appd: subview_act(T_SubView.Appd); break;
             }
             clear_dom_menu_list();
             display_default_message();
@@ -1003,16 +1218,27 @@ function isSL(): void {
     g_mvm.clear_message();
     switch (TG_mode) {
         case T_TG_mode.Team:
-            if (!exist_guld()) break;
-            TG_mode = T_TG_mode.Guld;
-            subview_act(T_SubView.Guld);
+            if (exist_guld()) {
+                TG_mode = T_TG_mode.Guld;
+                break;
+            }
+            TG_mode = T_TG_mode.Appd;
             break;
         case T_TG_mode.Guld:
-            if (!exist_team()) break;
-            TG_mode = T_TG_mode.Team;
-            subview_act(T_SubView.Team);
+            TG_mode = T_TG_mode.Appd;
+            break;
+        case T_TG_mode.Appd:
+            if (exist_team()) {
+                TG_mode = T_TG_mode.Team;
+                break;
+            }
+            if (exist_guld()) {
+                TG_mode = T_TG_mode.Guld;
+                break;
+            }
             break;
     }
+    subview_act(TG_mode);
     display_default_message();
 }
 function isRT(): void {
@@ -1043,6 +1269,12 @@ function display_default_message(): void {
             break;
         case 'fire':
             g_mvm.notice_message('ギルドをクビにしますか？クビにしたメンバーは復帰できません');
+            break;
+        case 'adpt':
+            g_mvm.notice_message('ギルドに採用しますか？');
+            break;
+        case 'away':
+            g_mvm.notice_message('応募者を追い返しますか？追い返したメンバーは復帰できません');
             break;
         default:
             g_mvm.clear_message();
