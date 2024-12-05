8c0df71239ade10d28ea864aab687dede5cd06b6
itsayno33 <itsayno33@gmail.com>
2024-12-05 13:27:36 +0900

    .

--- www.bat
+++ www.bat
@@ -1,1 +1,1 @@
-robocopy E:\Dev\vscode\mai\mai\src\html E:\Dev\vscode\mai\Mai_docker\www\html\mai  /MIR /EFSRAW /NP /V /R:1 /W:1 /IT /LOG+:www.log
+robocopy D:\Dev\ts\mai\src\html D:\Docker\Mai_Docker\www\html\mai  /MIR /EFSRAW /NP /V /R:1 /W:1 /IT /LOG+:www.log
