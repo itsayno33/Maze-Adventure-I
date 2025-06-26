61c6c13d9c2cbfc05aea6575ef4bdf0a0bee8fdc
itsayno33 <itsayno33@gmail.com>
2025-06-26 17:52:35 +0900

    GM機能の入力中１

--- src/d_mdl/I_Hope.ts
+++ src/d_mdl/I_Hope.ts
@@ -2,7 +2,7 @@
 
 import { C_PointDir } from "./C_PointDir"
 
-export type T_HopeKind =
+export type T_HopeReqKind =
      'Move'      // 移動
   |  'Turn'      // ターン
   |  'Up'        // 上へ
@@ -11,15 +11,35 @@ export type T_HopeKind =
   |  'Defense'   // 防御   
   |  'Quick'     // 回避
   |  'Item'       // アイテム使用
+  ;
+
+
+export type T_HopeResKind =
+     'Move'      // 移動できる
+  |  'Turn'      // ターンできる
+  |  'Block'     // 行動を阻止された
+  ;
 
 export interface I_HasHope {
-    has_hope: boolean,    // 希望行動の有無
-    hope:     T_HopeKind,     // 行動の種類
+    has_hope: boolean,      // 希望行動の有無
+    hope:     T_HopeReqKind,   // 行動の種類
 }
 
 export interface I_HopeAction extends I_HasHope {
     subj:    C_PointDir,    // 対象の指定(位置)
-    doOK:    ()=>void,   // 許可時の行動(関数)
-    doNG:    ()=>void,   // 不許可時の行動(関数)
+    doOK:    ()=>void,      // 許可時の行動(関数)
+    doNG:    ()=>void,      // 不許可時の行動(関数)
 }
+
+export interface I_HopeResponce {
+    ok:   boolean,         // 行動可否
+    hope: I_HopeAction,    // 行動の種類
+    res:  T_HopeResKind,   // 行動結果
+}
+
+export interface I_HopeResponceMove extends I_HopeResponce {
+    damage: number,        // ダメージ値(移動不可時の値)
+}
+
+export interface I_HopeResponceTurn extends I_HopeResponce {}
 
--- src/mai_maze/F_GM_judge.ts
+++ src/mai_maze/F_GM_judge.ts
@@ -1,5 +1,4 @@
 import { T_MzKind }                   from "../d_mdl/T_MzKind";
-import { I_HopeAction }               from "../d_mdl/I_Hope";
 import { C_Point }                    from "../d_mdl/C_Point";
 import { g_debug }                    from "../d_cmn/global";
 
@@ -10,41 +9,83 @@ import {
     g_team,
 } from "./global_for_maze";
 
+import { 
+    I_HopeAction, 
+    I_HopeResponceMove, 
+    I_HopeResponceTurn
+}               from "../d_mdl/I_Hope";
 
-// 移動時の判定
-export function jdge_move() {}
+
+// 移動時の判定   true: 移動可能  false: 移動不可
+export function can_move(r: I_HopeAction): I_HopeResponceMove {
+    const _touch = _can_touch(r);
+    if (!_touch) {
+        const _move  = _can_move(r);
+        return {
+            ok: _move > 0,    // 移動可否
+            hope: r,          // 希望行動
+            res: _move > 0 ? 'Move' : 'Block', // 行動結果
+            damage: _move,    // 移動不可時のダメージ値
+        }
+    } else {
+        return {
+            ok:      false,   // 移動不能
+            hope:    r,       // 希望行動
+            res:    'Block',  // 行動結果
+            damage:  0,       // 移動不可時のダメージ値
+        }
+    }
+}
 
 
-// ターン時の判定
-export function jdge_turn() {}
+export function jdge_move(r: I_HopeAction): void {
+    _jdge_collide(r);
+    _jdge_stepon(r);
+}
+
+
+// ターン時の判定  true: ターン可能  false: ターン不可
+export function can_turn(r: I_HopeAction): I_HopeResponceTurn {
+    const _turn =_can_turn(r);
+    if (_turn) {
+        const _touch =_can_touch(r);
+        return {ok: !_touch , hope: r, res: _touch ? 'Block' : 'Turn'}; // ターン可能
+    } else {
+        return {ok: false, hope: r, res: 'Block'}; // ターン不可
+    }
+}
+
+
+export function jdge_turn(r: I_HopeAction): void {}
 
 
 // 移動可否    ゼロ以下なら移動不可。その場合の値はダメージ値
-function _can_move(): number { return 1; }
+function _can_move(r: I_HopeAction): number { return 1; }
 
 // ターン可否  true:可, false:不可
-function _can_turn(): boolean { return true; }
+function _can_turn(r: I_HopeAction): boolean { return true; }
 
-// 当たり判定（接触）
-function _jdge_touch() {}
+// 当たり判定（接触）  true: 接触あり  false: 接触なし
+// ここでは接触はない設定
+function _can_touch(r: I_HopeAction): boolean { return false; }
 
 // 当たり判定（衝突）
-function _jdge_collide() {}
+function _jdge_collide(r: I_HopeAction): void {}
 
 // 当たり判定（上に乗る）
-function _jdge_stepon() {}
+function _jdge_stepon(r: I_HopeAction): void {}
 
 // 拾うものがあるか
-function _jdge_pickup(): boolean {
+function _jdge_pickup(r: I_HopeAction): boolean {
     return false; // ここでは拾うものはない
 }
 
 // 罠があるか
-function _jdge_trap(): boolean {
+function _jdge_trap(r: I_HopeAction): boolean {
     return false; // ここでは罠はない
 }
 
 // 敵がいるか
-function _jdge_enemy(): boolean {
+function _jdge_enemy(r: I_HopeAction): boolean {
     return false; // ここでは敵はいない
 }
