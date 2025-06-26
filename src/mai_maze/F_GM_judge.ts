f614ee44c9f138d0b7c9553e90b17fc71f340b0c
itsayno33 <itsayno33@gmail.com>
2025-06-26 17:57:24 +0900

    GM機能入力中2

--- src/mai_maze/F_GM_judge.ts
+++ src/mai_maze/F_GM_judge.ts
@@ -17,10 +17,10 @@ import {
 
 
 // 移動時の判定   true: 移動可能  false: 移動不可
-export function can_move(r: I_HopeAction): I_HopeResponceMove {
-    const _touch = _can_touch(r);
+export function hero_can_move(r: I_HopeAction): I_HopeResponceMove {
+    const _touch = _hero_can_touch(r);
     if (!_touch) {
-        const _move  = _can_move(r);
+        const _move  = _hero_can_move(r);
         return {
             ok: _move > 0,    // 移動可否
             hope: r,          // 希望行動
@@ -38,17 +38,17 @@ export function can_move(r: I_HopeAction): I_HopeResponceMove {
 }
 
 
-export function jdge_move(r: I_HopeAction): void {
-    _jdge_collide(r);
-    _jdge_stepon(r);
+export function hero_jdge_move(r: I_HopeAction): void {
+    _hero_jdge_collide(r);
+    _hero_jdge_stepon(r);
 }
 
 
 // ターン時の判定  true: ターン可能  false: ターン不可
-export function can_turn(r: I_HopeAction): I_HopeResponceTurn {
-    const _turn =_can_turn(r);
+export function hero_can_turn(r: I_HopeAction): I_HopeResponceTurn {
+    const _turn =_hero_can_turn(r);
     if (_turn) {
-        const _touch =_can_touch(r);
+        const _touch =_hero_can_touch(r);
         return {ok: !_touch , hope: r, res: _touch ? 'Block' : 'Turn'}; // ターン可能
     } else {
         return {ok: false, hope: r, res: 'Block'}; // ターン不可
@@ -56,36 +56,36 @@ export function can_turn(r: I_HopeAction): I_HopeResponceTurn {
 }
 
 
-export function jdge_turn(r: I_HopeAction): void {}
+export function hero_jdge_turn(r: I_HopeAction): void {}
 
 
 // 移動可否    ゼロ以下なら移動不可。その場合の値はダメージ値
-function _can_move(r: I_HopeAction): number { return 1; }
+function _hero_can_move(r: I_HopeAction): number { return 1; }
 
 // ターン可否  true:可, false:不可
-function _can_turn(r: I_HopeAction): boolean { return true; }
+function _hero_can_turn(r: I_HopeAction): boolean { return true; }
 
 // 当たり判定（接触）  true: 接触あり  false: 接触なし
 // ここでは接触はない設定
-function _can_touch(r: I_HopeAction): boolean { return false; }
+function _hero_can_touch(r: I_HopeAction): boolean { return false; }
 
 // 当たり判定（衝突）
-function _jdge_collide(r: I_HopeAction): void {}
+function _hero_jdge_collide(r: I_HopeAction): void {}
 
 // 当たり判定（上に乗る）
-function _jdge_stepon(r: I_HopeAction): void {}
+function _hero_jdge_stepon(r: I_HopeAction): void {}
 
 // 拾うものがあるか
-function _jdge_pickup(r: I_HopeAction): boolean {
+function _hero_jdge_pickup(r: I_HopeAction): boolean {
     return false; // ここでは拾うものはない
 }
 
 // 罠があるか
-function _jdge_trap(r: I_HopeAction): boolean {
+function _hero_jdge_trap(r: I_HopeAction): boolean {
     return false; // ここでは罠はない
 }
 
 // 敵がいるか
-function _jdge_enemy(r: I_HopeAction): boolean {
+function _hero_jdge_enemy(r: I_HopeAction): boolean {
     return false; // ここでは敵はいない
 }
