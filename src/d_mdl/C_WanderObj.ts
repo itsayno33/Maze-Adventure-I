3458c7b851c8796f0e13759d9cbf286d3c53730d
itsayno33 <itsayno33@gmail.com>
2025-07-05 07:54:48 +0900

    バグ取り継続中８ C_WanderObjクラスのコンストラクタからsetpd(),set_view(),new_walker()をdecode()に移動、統一

--- src/d_mdl/C_WanderObj.ts
+++ src/d_mdl/C_WanderObj.ts
@@ -13,7 +13,8 @@ export interface JSON_WanderObjSTAT extends JSON_MazeObjSTAT {
 }
 export interface JSON_WanderObj extends JSON_MazeObj {
     clname?:    string,
-    wdwalk?:    JSON_WanderWalker|undefined,
+    walk?:      JSON_WanderWalker|undefined,
+    view?:      JSON_WanderView|undefined,
     stat?:      JSON_WanderObjSTAT, // C_WanderObjのサブクラスの初期値を保持する
 }
 
@@ -29,27 +30,25 @@ export class C_WanderObj  extends C_MazeObj implements I_MazeObj {
         this.can_thr = true; // 壁を通過可能
         this.h_w_dmg = 0;    // 壁に当たったときのダメージ無し
 
-        const loc_pos    = new C_PointDir(j?.loc_pos ?? {x:1, y:1, z:0, d:0});
-        this.set_pd(loc_pos);
+        j ??= {} as JSON_WanderObj; // jが未定義の場合は空のオブジェクトを用意
 
 
-        // Viewの初期化
-        // ViewはJSON_WanderWalkerViewを使用して初期化する
-        const view = new C_WanderView({
+        // loc_posが未定義の場合は初期位置を設定。decode(j)でthis.set_pd()を呼び出す
+        if (j.loc_pos === undefined) j.loc_pos ??= j.wdwalk.loc_pos ?? {x:1, y:1, z:0, d:0}; 
+
+        // viewが未定義の場合はこれを初期化
+        // Viewはdecode(j)でJSON_WanderWalkerViewを使用して生成する
+        j.view ??= {
                 layer: 0, letter: '漂', 
                 show3D:  '1',
                 pad_t: 0.2, pad_d: 0.0, pad_s: 0.38,
                 col_f: '#ff99ff', col_b: '#dd88dd', col_s: '#dd88dd', col_t: '#dd88dd', col_d: '#dd88dd', 
                 col_l: '#9999ff', col_2: '#ff33ff', col_L: '#6666ff', 
                 col_2_arw: '#338866', col_2_tri: '#cc6666',
-        } as JSON_WanderView);
-        this.setView(view);
+        } as JSON_WanderView;
         
-        // Walkerの初期化
-        // loc_posはC_WanderWalkerの初期位置に使用される
-        this.wdwalk      = new_walker(loc_pos);              // new C_WanderWalker(loc_pos)を使用して初期化？？？
-//        this.wdwalk.set_pd(loc_pos);
-        this.wdwalk.set_mazeObj(this); // Walkerに自分自身を設定
+        // Walkerの初期化。decode(j)にて新しいC_WanderWalkerを生成
+        j.wdwalk ??= {} as JSON_WanderWalker; // 初期化
 
         if (j !== undefined) this.decode(j);
     }
@@ -65,11 +64,22 @@ export class C_WanderObj  extends C_MazeObj implements I_MazeObj {
         j.stat.wo  = {dmy: this.dmy}; // ダミー変数
         return j;
     }
-    public decode(j: JSON_WanderObj): C_WanderObj {
-        super.decode(j)
-        if (j.clname   !== undefined) this.clname    = j.clname;
-        if (j.wdwalk   !== undefined) this.wdwalk    = new_walker(j.wdwalk);
-        if (j.stat?.wo !== undefined) {
+    public decode(j: JSON_WanderObj): C_MazeObj {
+        super.decode(j);
+        if (j?.clname   !== undefined) this.clname    = j.clname;
+
+        // loc_posが未定義の場合は初期位置を設定
+        if (j?.loc_pos  === undefined && j.wdwalk.loc_pos  !== undefined)  j.loc_pos = j.wdwalk.loc_pos; 
+        if (j?.loc_pos  !== undefined)  this.set_pd(j.loc_pos); 
+
+        if (j?.view     !== undefined) this.setView(new C_WanderView(j.view));
+
+        if (j?.wdwalk   !== undefined) {
+            j.wdwalk.loc_pos ??= j?.loc_pos ?? {x:1, y:1, z:0, d:0}
+            this.wdwalk        = new_walker(j.wdwalk);
+        }
+
+        if (j?.stat?.wo !== undefined) {
             this.dmy = j.stat.wo.dmy ?? ''; // ダミー変数
         }
         return this;
