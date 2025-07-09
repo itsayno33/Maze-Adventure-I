ec4bedff703c882899e0c7c1bd608ea073c22a16
itsayno33 <itsayno33@gmail.com>
2025-07-09 15:47:36 +0900

    メモリリークっぽいやつの修正？

--- src/mai_maze/C_HresInfo.ts
+++ src/mai_maze/C_HresInfo.ts
@@ -112,14 +112,12 @@ export class C_HresInfo {
         this.info[idx] ??= {} as _T_HeroInfo;
 
         if (idx < this.myHres.length) {
-            this.info[idx] = {
-                name: this.myHres[idx].name(),
-                stat: this.myHres[idx].is_alive() ? '　正常　' : '行動不能',
-                nwHp: this.myHres[idx].get_abi_p_now("xp") - this.myHres[idx].get_abi_p_now("xd"),
-                mxHp: this.myHres[idx].get_abi_p_ttl("xp"),
-                nwMp: this.myHres[idx].get_abi_m_now("xp") - this.myHres[idx].get_abi_m_now("xd"),
-                mxMp: this.myHres[idx].get_abi_m_ttl("xp"),
-            }
+            this.info[idx].name = this.myHres[idx].name();
+            this.info[idx].stat = this.myHres[idx].is_alive() ? '　正常　' : '行動不能';
+            this.info[idx].nwHp = this.myHres[idx].get_abi_p_now("xp") - this.myHres[idx].get_abi_p_now("xd");
+            this.info[idx].mxHp = this.myHres[idx].get_abi_p_ttl("xp");
+            this.info[idx].nwMp = this.myHres[idx].get_abi_m_now("xp") - this.myHres[idx].get_abi_m_now("xd");
+            this.info[idx].mxMp = this.myHres[idx].get_abi_m_ttl("xp");
         } else {
             this.info[idx] = undefined;
         }
