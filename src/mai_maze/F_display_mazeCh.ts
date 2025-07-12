38afc0b58a5091a1c12dc784061cdbbd41759179
itsayno33 <itsayno33@gmail.com>
2025-07-12 16:30:26 +0900

    迷宮の文字マップが小さい教練場でマップ表示がズレるバグを解消

--- src/mai_maze/F_display_mazeCh.ts
+++ src/mai_maze/F_display_mazeCh.ts
@@ -38,6 +38,19 @@ function calc_viewCh_width(): void {
     map_wdth    = font_size * col;
     map_hght    = line_hght * col;
 
+
+
+    if (map_wdth   < view_wdth) {
+        map_wdth   = view_wdth;
+        font_size  = _round(view_wdth / col, 0);
+    }
+    if (map_hght   < view_hght) {
+        map_hght   = view_hght;
+        line_hght  = _round(view_hght / row, 0);
+    }
+
+
+
     pre.setAttribute('width',  map_wdth.toString());
     pre.setAttribute('height', map_hght.toString());
     pre.style.setProperty('font-size',  `${font_size}px`);
@@ -45,19 +58,22 @@ function calc_viewCh_width(): void {
 }
 
 function calc_viewCh_top(): void {
+    calc_viewCh_width();
 
     view_wdth  = div.clientWidth;
     view_hght  = div.clientHeight;
 
     const pd = g_team.get_pd();
 
-    let top_x =  view_wdth / 2 - (pd.x + 1) * font_size;
+    let top_x =  view_wdth / 2 - (pd.x + 0) * font_size;
     if (top_x > 0) top_x = 0; // 左端制限
     if (top_x < view_wdth - map_wdth) top_x = view_wdth - map_wdth; // 右端制限
 
-    let top_y =  view_hght / 2 - (pd.y + 1) * line_hght;
+    let top_y =  view_hght / 2 - (pd.y + 0) * line_hght;
     if (top_y > 0) top_y = 0; // 上端制限
     if (top_y < view_hght - map_hght) top_y = view_hght - map_hght; // 下端制限
+                                                                                //alert(`ViewCh: ${view_wdth}x${view_hght} px, map: ${map_wdth}x${map_hght} px`);
+                                                                                //alert(`top_x: ${top_x}, top_y: ${top_y}`);
 
     pre.style.setProperty('left',      `${top_x}px`);
     pre.style.setProperty('top',       `${top_y}px`);
